1f546da8aad2011603786f2c029b1ecad68d1c32
TS-SCRIPTS <103532607+TS-SCRIPTS@users.noreply.github.com>
2023-02-21 17:18:36 +0100

    Add files via upload

---
+++ esx_menu_default/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+-- 
+
+description 'By TS#3462'
+
+--
+
+files {
+	'**/**/**/*.*'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+client_scripts {
+	'client/main.lua'
+}
